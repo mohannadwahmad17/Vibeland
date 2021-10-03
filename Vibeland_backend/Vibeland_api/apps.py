cafedc9cc0b7873c0e1cd595e0ba2ec0bbf0fda1
mohannadwahmad17 <mohannadwahmad97@gmail.com>
2021-10-03 15:17:42 -0500

    finished setting up django

---
+++ Vibeland_backend/Vibeland_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VibelandApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Vibeland_api'
---
+++ Vibeland_backend/Vibeland_api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from . import views
+from django.urls import path
+
+url_patterns = [
+    
+]
