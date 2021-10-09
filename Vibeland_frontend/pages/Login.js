76438e72753b265f4a397fe9e0d5a98a56a901c9
mohannadwahmad17 <mohannadwahmad97@gmail.com>
2021-10-08 23:05:36 -0500

    installed nativebase

--- Vibeland_frontend/pages/Login.js
+++ Vibeland_frontend/pages/Login.js
@@ -1,37 +1,83 @@
-import React from 'react'
-import { useState } from "react";
-import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+import * as React from 'react';
+import { MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
+import {
+  NativeBaseProvider,
+  Box,
+  Text,
+  Heading,
+  VStack,
+  FormControl,
+  Input,
+  Link,
+  Button,
+  Icon,
+  IconButton,
+  HStack,
+  Divider,
+} from 'native-base';
 
 const LoginPage = () => {
-    const [username, onChangeUsername] = React.useState(null);
-    const [password, onChangePassword] = React.useState(null);
+  return (
+    <NativeBaseProvider>
+      <Box safeArea flex={1} p="2" py="8" w="90%" mx="auto">
+        <Heading size="lg" fontWeight="600" color="coolGray.800">
+          Welcome
+        </Heading>
+        <Heading mt="1" color="coolGray.600" fontWeight="medium" size="xs">
+          Sign in to continue!
+        </Heading>
 
-    return (
-        <SafeAreaView>
-            <TextInput
-                style={styles.input}
-                onChangeText={onChangeUsername}
-                value={username}
-                placeholder="Enter Username"
-            />
-            <TextInput
-                style={styles.input}
-                onChangeText={onChangePassword}
-                value={password}
-                placeholder="Enter Password"
-            />
-        </SafeAreaView>
-    );
+        <VStack space={3} mt="5">
+          <FormControl>
+            <FormControl.Label
+              _text={{
+                color: 'coolGray.800',
+                fontSize: 'xs',
+                fontWeight: 500,
+              }}>
+              Email ID
+            </FormControl.Label>
+            <Input />
+          </FormControl>
+          <FormControl>
+            <FormControl.Label
+              _text={{
+                color: 'coolGray.800',
+                fontSize: 'xs',
+                fontWeight: 500,
+              }}>
+              Password
+            </FormControl.Label>
+            <Input type="password" />
+            <Link
+              _text={{ fontSize: 'xs', fontWeight: '500', color: 'indigo.500' }}
+              alignSelf="flex-end"
+              mt="1">
+              Forget Password?
+            </Link>
+          </FormControl>
+          <Button mt="2" colorScheme="indigo" _text={{ color: 'white' }}>
+            Sign in
+          </Button>
+          <HStack mt="6" justifyContent="center">
+            <Text fontSize="sm" color="muted.700" fontWeight={400}>
+              I'm a new user.{' '}
+            </Text>
+            <Link
+              _text={{
+                color: 'indigo.500',
+                fontWeight: 'medium',
+                fontSize: 'sm',
+              }}
+              href="#">
+              Sign Up
+            </Link>
+          </HStack>
+        </VStack>
+      </Box>
+    </NativeBaseProvider>
+  );
 };
-
-const styles = StyleSheet.create({
-    input: {
-        height: 40,
-        margin: 12,
-        borderWidth: 1,
-        padding: 10,
-    },
-});
 
 export {
     LoginPage
