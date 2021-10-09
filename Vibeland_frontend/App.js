f54e45aa9cc33a1c2b8eae245713e942d7075755
mohannadwahmad17 <mohannadwahmad97@gmail.com>
2021-10-08 23:08:54 -0500

    added sign up page

--- Vibeland_frontend/App.js
+++ Vibeland_frontend/App.js
@@ -11,6 +11,7 @@ import type {Node} from 'react';
 import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, TextInput, useColorScheme, View } from 'react-native';
 import { Colors, DebugInstructions, Header, LearnMoreLinks, ReloadInstructions } from 'react-native/Libraries/NewAppScreen';
 import { LoginPage } from './pages/Login';
+import { Signup } from './pages/SignUp'
 
 const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
