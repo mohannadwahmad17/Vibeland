8cf0c94e21a99ab989f8959436deb9c3294caed7
mohannadwahmad17 <mohannadwahmad97@gmail.com>
2021-10-15 16:27:29 -0500

    included firebase credentials and verified spotify user authnetication works

--- Vibeland_backend/Vibeland_api/constants/constants.py
+++ Vibeland_backend/Vibeland_api/constants/constants.py
@@ -1,10 +1,9 @@
-MY_USERNAME = "wcxn4qe8j3xcf3lx6ks81y2nh"
-CLIENT_ID = "4aadd13a67674e16adb17e0dc4405e44"
-CLIENT_SECRET = "b8cc2bbdff004097940d2c7db8f4bc19"
-SPOTIPY_REDIRECT_URI = "http://localhost:8080"
-
-
 LIBRARY_SCOPES = "user-library-read"
-USER_SCOPES = "user-read-private user-read-email"
-SPOTIFY_CONNECT_SCOPES = "user-read-playback-state user-read-currently-playing"
-LISTENING_HISTORY_SCOPES = "user-read-playback-position user-read-recently-played user-top-read"
+USER_SCOPES = "user-read-private, user-read-email"
+SPOTIFY_CONNECT_SCOPES = "user-read-playback-state, user-read-currently-playing"
+LISTENING_HISTORY_SCOPES = "user-read-playback-position, user-read-recently-played, user-top-read"
+
+#Database node references
+FIREBASE_ROOT_NODE_REF = "/"
+FIREBASE_USERS_NODE_NAME = "/Users"
+FIREBASE_USER_TOKEN_REF = "/access_token"
--- Vibeland_backend/Vibeland_api/spotify_control_center.py
+++ Vibeland_backend/Vibeland_api/spotify_control_center.py
@@ -1,6 +1,8 @@
 import spotipy
 import spotipy.util as util
 from spotipy.client import Spotify
+from Vibeland_api.firebase import *
+from Vibeland_api.secrets.secrets import *
 from Vibeland_api.constants.constants import *
 from spotipy.oauth2 import SpotifyClientCredentials
 
@@ -10,13 +12,18 @@ def clientCredentialsFlow():
     client_credentials_manager = SpotifyClientCredentials(CLIENT_ID, CLIENT_SECRET)
     spotify_api_accessor = Spotify(client_credentials_manager)
 
+#This obtains the user authentication token to access user Spotify information
 def authorizationCodeFlow(credentials):
-    clientCredentialsFlow()
-
-    scopes = USER_SCOPES + " " + SPOTIFY_CONNECT_SCOPES + " " +  LIBRARY_SCOPES + " " + LISTENING_HISTORY_SCOPES
+    scopes = USER_SCOPES + ", " + SPOTIFY_CONNECT_SCOPES + ", " +  LIBRARY_SCOPES + ", " + LISTENING_HISTORY_SCOPES
     token = util.prompt_for_user_token(username=credentials["username"], scope=scopes, 
         client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI)
 
-    if token:
-        spotify = spotipy.Spotify(auth=token)
-        results = spotify.current_user_saved_tracks()
+    try:
+        if token:
+            spotify_api_accessor = spotipy.Spotify(auth=token)
+            return spotify_api_accessor
+        else:
+            return "Error with access token!"
+    except:
+        print("Error authentication access token!")
+        return None
