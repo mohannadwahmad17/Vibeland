d8977f72f395aa8389e9b353226aacb5defa0195
mohannadwahmad17 <mohannadwahmad97@gmail.com>
2021-10-21 13:00:58 -0500

    package json

--- Vibeland_backend/Vibeland_api/recommender/recommend_engine.py
+++ Vibeland_backend/Vibeland_api/recommender/recommend_engine.py
@@ -3,5 +3,9 @@ def recommendationEngine(spotifyApiAccessor):
     #Get the current user's song library
     user_song_library = getUserSongLibrary(spotifyApiAccessor)
 
+    print(user_song_library)
+
 def getUserSongLibrary(spotifyApiAccessor):
-    pass
+    user_library = spotifyApiAccessor.get_current_user_saved_tracks()
+
+    return user_library
