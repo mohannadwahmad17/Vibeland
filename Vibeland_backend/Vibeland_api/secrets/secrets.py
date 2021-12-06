2a51915005508aabfe48388e8dd97b66723a252b
mohannadwahmad17 <mohannadwahmad97@gmail.com>
2021-12-06 13:10:01 -0600

    a

---
+++ Vibeland_backend/Vibeland_api/secrets/secrets.py
@@ -0,0 +1,7 @@
+MY_USERNAME = "wcxn4qe8j3xcf3lx6ks81y2nh"
+CLIENT_ID = "4aadd13a67674e16adb17e0dc4405e44"
+CLIENT_SECRET = "b8cc2bbdff004097940d2c7db8f4bc19"
+SPOTIPY_REDIRECT_URI = "http://localhost:8080"
+
+PATH_TO_FIREBASE_SERVICE_ACCOUNT_KEY = "secrets/vibeland-f74df-firebase-adminsdk-kszww-40311567ab.json"
+FIREBASE_DATABASE_URL = "https://vibeland-f74df-default-rtdb.firebaseio.com/"
